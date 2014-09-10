23124d60e893f7518573f4f1dad1398eb457b2b6
Joseph Chow <sortofsleepy@gmail.com>
2014-09-10 11:52:46 -0700

    Re-wrote script to split tasks as well as add in the conversion from .po to something js ready. Started re-working search.

---
+++ public/js/services/MasterSearch.js
@@ -0,0 +1,18 @@
+/**
+ * @ngdoc service
+ * @name FirefoxMarket.service:MasterSearch
+ *
+ * @description
+ * This is the service that pings the API when it comes to search and filters
+ *
+ */
+app.factory("MasterSearch",function(){
+
+    var API = {
+
+
+    };
+
+
+    return API;
+});
--- public/translation.sh
+++ public/translation.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
-#extract all the nodes that need translating into .pot files
-grunt nggettext_extract
+#this script serves 2 purposes
+# 1 . It can extract all the strings in the templates marked for translation
+# 2. It can also take .pot files and convert them into .po files
+# 3. It finally can build the .po files into something useable by angular.
 
-cd pot
-for f in *.pot;
-do
-    echo "Processing $f file..."
-    msginit --input=$f --output="../po/${f%.*}.po"
+while getopts ":ecb:" opt; do
+  case $opt in
+    e)
+        #extract all the nodes that need translating into .pot files
+        grunt nggettext_extract
+      ;;
+    c)
+        echo "here"
+        cd pot
+            for f in *.pot;
+            do
+            echo "Processing $f file..."
+            msginit --input=$f --output="../po/${f%.*}.po"
+        done
+    ;;
+
+    b)
+        grunt nggettext_compile
+    ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
 done
+
+exit 0
